b250ba253bbad1d4c1d9f082a5d9bf0a494c1fdd
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2021-08-13 13:41:49 +0530

    Update send_notifications.6.x.py

--- quickstart/python/sms/example-1/send_notifications.6.x.py
+++ quickstart/python/sms/example-1/send_notifications.6.x.py
@@ -1,16 +1,14 @@
-# /usr/bin/env python
-# Download the twilio-python library from twilio.com/docs/libraries/python
-import os
+
 from twilio.rest import Client
 
 # Find these values at https://twilio.com/user/account
 # To set up environmental variables, see http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
+account_sid = 'AC84393935d48f1ee510aa169e3bbc78ef'
+auth_token = '36c7c1c98479471bcfe5a7ac2472ce1b'
 
 client = Client(account_sid, auth_token)
 
 client.api.account.messages.create(
-    to="+12316851234",
-    from_="+15555555555",
+    to="+9170123344444",
+    from_="+14195044686",
     body="Hello there!")
